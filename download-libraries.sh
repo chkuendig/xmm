27bed1c3e1644437b2b0da09f30d8902e06de638
mihmig <mihmig@a86212f3-689c-4790-bbe1-32f6eb5c469e>
2007-04-30 16:15:45 +0000

    - added PropertiesLayout.jar to load library in run.sh - added script to download and unpack required libraries to MovieManager/lib from current source package
    git-svn-id: svn://svn.code.sf.net/p/xmm/svn/trunk/MeDs@279 a86212f3-689c-4790-bbe1-32f6eb5c469e

---
+++ download-libraries.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "This script downloads the necessary libraries to run MeDs MovieManager."
+
+# Get current version from MovieManager.java to compose filename for sources
+VERFILE=`find . | grep /MovieManager.java`
+CURRENTVERSION=`grep "String _version" $VERFILE | awk 'BEGIN { FS = "\"" } { print $2 }'`
+
+echo "Current CVS version ${CURRENTVERSION}"
+
+# Compose download URL
+ZIPFILE="MovieManager.v.${CURRENTVERSION}.src.zip"
+URL="http://downloads.sourceforge.net/xmm/${ZIPFILE}?download"
+
+echo
+echo "Starting download for $URL"
+echo
+wget -c $URL
+
+# MovieManager/lib is in a subdirectory in the ZIP file, whose name may change
+# -> get name and create link to copy to correct destination
+LINKTARGET=`unzip -l $ZIPFILE '*/MovieManager/lib/' | grep /MovieManager/lib/ | awk 'BEGIN { FS = " " } { sub("/MovieManager/lib/", "", $4) ; print $4 }'`
+
+ln -s . $LINKTARGET
+
+# Unzip files in MovieManager/lib/
+unzip -n $ZIPFILE '*/MovieManager/lib/*'
+
+rm $LINKTARGET
+
+echo "Done. Now compile and run."
