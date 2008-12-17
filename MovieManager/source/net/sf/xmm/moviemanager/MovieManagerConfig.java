c6a0aeba1fc5f49d0703697ac2d7ac59aab75dbf
bro3 <bro3@a86212f3-689c-4790-bbe1-32f6eb5c469e>
2008-12-17 12:24:39 +0000

    Changed to v2.7
    git-svn-id: svn://svn.code.sf.net/p/xmm/svn/trunk/MeDs@471 a86212f3-689c-4790-bbe1-32f6eb5c469e

--- MovieManager/source/net/sf/xmm/moviemanager/MovieManagerConfig.java
+++ MovieManager/source/net/sf/xmm/moviemanager/MovieManagerConfig.java
@@ -666,7 +666,7 @@ public class MovieManagerConfig implements NewDatabaseLoadedEventListener {
 		  /**
 	     * The current version of the program.
 	     **/
-	    private static final String _version = " 2.7 beta 1"; //$NON-NLS-1$
+	    private static final String _version = " 2.7"; //$NON-NLS-1$
 	    
 	    
 	    String appTitle = " MeD's Movie Manager v" + getVersion();
