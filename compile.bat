c0ae2920764260962a1f9b0ca0b5f8cf7b5b48c4
bro3 <bro3@a86212f3-689c-4790-bbe1-32f6eb5c469e>
2009-01-10 22:01:13 +0000

    Works for v2.7.2
    git-svn-id: svn://svn.code.sf.net/p/xmm/svn/trunk/MeDs@544 a86212f3-689c-4790-bbe1-32f6eb5c469e

--- compile.bat
+++ compile.bat
@@ -2,5 +2,45 @@
 @echo Deleting files...
 @del /Q/S net > nul
 @echo Compiling...
-javac -d . -target 1.4 -source 1.4 -classpath "%CLASSPATH%";.;lib/LookAndFeels/skinlf.jar;lib/LookAndFeels/oalnf.jar;lib/TableLayout.jar;lib/useful.jar;lib/BrowserLauncher2-10rc4.jar;lib/jxl.jar;lib/JNative.jar;lib/retroweaver-all-2.0.jar;lib/commons-httpclient-3.0.1.jar;lib/log4j-1.2.13.jar;lib/commons-beanutils.jar;lib/commons-collections-3.2.jar;lib/commons-digester-1.7.jar;lib/itext-1.4.5.jar;lib/jasperreports-3.0.0.jar;lib/poi-2.5.1-final-20040804.jar;lib/castor-1.0.5.jar;lib/ProportionLayout.jar;lib/driveinfo/sfc.jar;lib/ostermillerutils_1_05_00_for_java_1_4.jar;lib/cobra/cobra-0.98.2.jar;lib/cobra/js.jar  source/net/sf/xmm/moviemanager/*.java source/net/sf/xmm/moviemanager/database/*.java source/net/sf/xmm/moviemanager/swing/extentions/*.java source/net/sf/xmm/moviemanager/swing/extentions/events/*.java source/net/sf/xmm/moviemanager/swing/extentions/filetree/*.java source/net/sf/xmm/moviemanager/swing/util/table/*.java source/net/sf/xmm/moviemanager/fileproperties/*.java source/net/sf/xmm/moviemanager/gui/*.java source/net/sf/xmm/moviemanager/util/*.java source/net/sf/xmm/moviemanager/http/*.java source/net/sf/xmm/moviemanager/models/*.java source/net/sf/xmm/moviemanager/commands/*.java  source/net/sf/xmm/moviemanager/commands/guistarters/*.java source/net/sf/xmm/moviemanager/commands/importexport/*.java source/net/sf/xmm/moviemanager/mediainfodll/*.java
+javac -d . -target 1.4 -source 1.4 -classpath "%CLASSPATH%";.;^
+lib/LookAndFeels/skinlf.jar;^
+lib/LookAndFeels/oalnf.jar;^
+lib/TableLayout.jar;^
+lib/useful.jar;^
+lib/BrowserLauncher2-10rc4.jar;^
+lib/jxl.jar;^
+lib/JNative.jar;^
+lib/retroweaver-all-2.0.jar;^
+lib/commons-httpclient-3.0.1.jar;^
+lib/log4j-1.2.13.jar;^
+lib/commons-beanutils.jar;^
+lib/commons-collections-3.2.jar;^
+lib/commons-digester-1.7.jar;^
+lib/itext-1.4.5.jar;^
+lib/jasperreports-3.0.0.jar;^
+lib/poi-2.5.1-final-20040804.jar;^
+lib/castor-1.2.jar;^
+lib/ProportionLayout.jar;^
+lib/driveinfo/sfc.jar;^
+lib/ostermillerutils_1_05_00_for_java_1_4.jar;^
+lib/cobra/cobra-0.98.2.jar;^
+lib/cobra/js.jar^
+ source/net/sf/xmm/moviemanager/*.java^
+ source/net/sf/xmm/moviemanager/database/*.java^
+ source/net/sf/xmm/moviemanager/swing/extentions/*.java^
+ source/net/sf/xmm/moviemanager/swing/extentions/events/*.java^
+ source/net/sf/xmm/moviemanager/swing/extentions/filetree/*.java^
+ source/net/sf/xmm/moviemanager/swing/util/table/*.java^
+ source/net/sf/xmm/moviemanager/fileproperties/*.java^
+ source/net/sf/xmm/moviemanager/gui/*.java^
+ source/net/sf/xmm/moviemanager/gui/menubar/*.java^
+ source/net/sf/xmm/moviemanager/util/*.java^
+ source/net/sf/xmm/moviemanager/util/plugins/*.java^
+ source/net/sf/xmm/moviemanager/http/*.java^
+ source/net/sf/xmm/moviemanager/models/*.java^
+ source/net/sf/xmm/moviemanager/models/imdb/*.java^
+ source/net/sf/xmm/moviemanager/commands/*.java^
+ source/net/sf/xmm/moviemanager/commands/guistarters/*.java^
+ source/net/sf/xmm/moviemanager/commands/importexport/*.java^
+ source/net/sf/xmm/moviemanager/mediainfodll/*.java
 @cd ..
--- run.bat
+++ run.bat
@@ -1,4 +1,35 @@
 @cd MovieManager
-java -classpath "%CLASSPATH%";.;lib\drivers\hsqldb-1.7.3.3.jar;lib\drivers\mysql-connector-java-3.1.12-bin.jar;lib\LookAndFeels\skinlf.jar;lib\LookAndFeels\oalnf.jar;lib\TableLayout.jar;lib\useful.jar;lib\BrowserLauncher2-10rc4.jar;lib\jxl.jar;lib\JNative.jar;lib\retroweaver-all-2.0.jar;lib\commons-httpclient-3.0.1.jar;lib\commons-logging-1.1.jar;lib\commons-codec-1.3.jar;lib\log4j-1.2.13.jar;lib\commons-beanutils.jar;lib\commons-collections-3.2.jar;lib\commons-digester-1.7.jar;lib\itext-1.4.5.jar;lib\jasperreports-3.0.0.jar;lib\jcommon-1.0.5.jar;lib\poi-2.5.1-final-20040804.jar;lib\castor-1.0.5.jar;lib\xercesImpl.jar;lib\xml-apis.jar;lib\ProportionLayout.jar;lib\driveinfo\sfc.jar;lib\ostermillerutils_1_05_00_for_java_1_4.jar;lib\cobra\cobra-0.98.2.jar;lib\cobra\js.jar  net.sf.xmm.moviemanager.MovieManager
+java -classpath "%CLASSPATH%";.;^
+lib\drivers\hsqldb-1.7.3.3.jar;^
+lib\drivers\mysql-connector-java-3.1.12-bin.jar;^
+lib\LookAndFeels\skinlf.jar;^
+lib\LookAndFeels\oalnf.jar;^
+lib\TableLayout.jar;^
+lib\useful.jar;^
+lib\BrowserLauncher2-10rc4.jar;^
+lib\jxl.jar;^
+lib\JNative.jar;^
+lib\retroweaver-all-2.0.jar;^
+lib\commons-httpclient-3.0.1.jar;^
+lib\commons-logging-1.1.jar;^
+lib\commons-codec-1.3.jar;^
+lib\log4j-1.2.13.jar;^
+lib\commons-beanutils.jar;^
+lib\commons-collections-3.2.jar;^
+lib\commons-digester-1.7.jar;^
+lib\itext-1.4.5.jar;^
+lib\jasperreports-3.0.0.jar;^
+lib\jcommon-1.0.5.jar;^
+lib\poi-2.5.1-final-20040804.jar;^
+lib\castor-1.2.jar;^
+lib\xercesImpl.jar;^
+lib\gettext-commons-0.9.jar;^
+lib\xml-apis.jar;^
+lib\ProportionLayout.jar;^
+lib\ostermillerutils_1_05_00_for_java_1_4.jar;^
+lib\cobra\cobra-0.98.2.jar;^
+lib\cobra\js.jar;^
+lib\driveinfo\sfc.jar;^
+ net.sf.xmm.moviemanager.MovieManager
 @cd ..
 
