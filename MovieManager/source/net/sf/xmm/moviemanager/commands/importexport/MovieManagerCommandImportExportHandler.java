275f4d258713060f788ab5ef737df4bf8e5ae248
bro3 <bro3@a86212f3-689c-4790-bbe1-32f6eb5c469e>
2009-01-10 00:57:41 +0000

    Specifies which list to add to, the regular or 'importer-skipped' list. NOw implements 	isCancelled() and isAborted()
    git-svn-id: svn://svn.code.sf.net/p/xmm/svn/trunk/MeDs@520 a86212f3-689c-4790-bbe1-32f6eb5c469e

--- MovieManager/source/net/sf/xmm/moviemanager/commands/importexport/MovieManagerCommandImportExportHandler.java
+++ MovieManager/source/net/sf/xmm/moviemanager/commands/importexport/MovieManagerCommandImportExportHandler.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import net.sf.xmm.moviemanager.MovieManager;
 import net.sf.xmm.moviemanager.gui.DialogAlert;
 import net.sf.xmm.moviemanager.gui.DialogIMDB;
+import net.sf.xmm.moviemanager.models.ModelImportExportSettings;
 import net.sf.xmm.moviemanager.models.ModelMovie;
 import net.sf.xmm.moviemanager.models.ModelMovieInfo;
 import net.sf.xmm.moviemanager.util.GUIUtil;
@@ -20,35 +21,37 @@ public class MovieManagerCommandImportExportHandler {
 	
 	ModelMovieInfo modelMovieInfo = new ModelMovieInfo(false, true);
 	ModelMovie movie = null;
+	
+	public String listToAddMovieTo = null;
+	
 	public ArrayList movieList = null;
 	
-	public void execute() throws Exception {
-		throw new Exception("MovieManagerCommandImportExportHandler does not implement method execute!");
-	}
 	
 	public void setCancelled(boolean cancel) {
-		System.err.println("setCancelled:" + cancel);
 		cancelled = cancel;
 		modelMovieInfo.clearModel();
 	}
 		
 	public void setAborted(boolean abort) {
-		System.err.println("setAborted:" + abort);
 		aborted = abort;
 	}
 		
+	public boolean isCancelled() {
+		return cancelled;
+	}
+	
+	public boolean isAborted() {
+		return aborted;
+	}
+	
+	public void execute() throws Exception {
+		throw new Exception("MovieManagerCommandImportExportHandler does not implement method execute!");
+	}
+	
 	public void done() throws Exception {
 		throw new Exception("MovieManagerCommandImportExportHandler does not implement method done!");
 	}
-	
-	public boolean isCancelled() throws Exception {
-		throw new Exception("MovieManagerCommandImportExportHandler does not implement method isCancelled!");
-	}
-	
-	public boolean isAborted() throws Exception {
-		throw new Exception("MovieManagerCommandImportExportHandler does not implement method isAborted!");
-	}
-	
+		
 	public int addMovie(int i) throws Exception {
 		throw new Exception("MovieManagerCommandImportExportHandler does not implement method addMovie!");
 	}
@@ -73,31 +76,28 @@ public class MovieManagerCommandImportExportHandler {
 	/**
      * Gets the IMDB info for movies (multiAdd)
      **/
-    public void executeCommandGetIMDBInfoMultiMovies(String searchString, String filename, int multiAddSelectOption, ModelMovie model) {
-        
+    public void executeCommandGetIMDBInfoMultiMovies(String searchString, String filename, ModelImportExportSettings settings, ModelMovie model) {
+      	
         /* Checks the movie title... */
         log.debug("executeCommandGetIMDBInfoMultiMovies"); //$NON-NLS-1$
+        
         if (!searchString.equals("")) { //$NON-NLS-1$
-            DialogIMDB dialogIMDB = new DialogIMDB(model, searchString, filename, null, multiAddSelectOption, null);
-            
-            System.err.println("dialogIMDB.cancelSet:" + dialogIMDB.cancelSet);
-            System.err.println("dialogIMDB.dropImdbInfoSet:" + dialogIMDB.dropImdbInfoSet);
-            System.err.println("dialogIMDB.cancelAllSet:" + dialogIMDB.cancelAllSet);
-            
-            if (dialogIMDB.cancelSet/* || dialogIMDB.dropImdbInfoSet*/)
+            DialogIMDB dialogIMDB = new DialogIMDB(model, searchString, filename, null, settings.multiAddIMDbSelectOption, null);
+             
+            if (dialogIMDB.cancelSet)
             	setCancelled(true);
             
             if (dialogIMDB.cancelAllSet)
             	setAborted(true);
             
-            
-            System.err.println("DirectedBy:" + model.getDirectedBy());
-           
-            
+            if (dialogIMDB.dropImdbInfoSet)
+            	listToAddMovieTo = settings.skippedListName;
+            else
+            	listToAddMovieTo = settings.addToThisList;
+                        
         } else {
             DialogAlert alert = new DialogAlert(MovieManager.getDialog(), Localizer.getString("DialogMovieInfo.alert.title.alert"), Localizer.getString("DialogMovieInfo.alert.message.please-specify-movie-title")); //$NON-NLS-1$ //$NON-NLS-2$
             GUIUtil.showAndWait(alert, true);
         }
     }
-    
 }
