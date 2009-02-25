a2bb2642a9127d32d39712870fc526dd5170fbe0
bro3 <bro3@a86212f3-689c-4790-bbe1-32f6eb5c469e>
2009-02-25 20:44:19 +0000

    Cleaned up
    git-svn-id: svn://svn.code.sf.net/p/xmm/svn/trunk/MeDs@657 a86212f3-689c-4790-bbe1-32f6eb5c469e

--- MovieManager/source/net/sf/xmm/moviemanager/util/mac/OSXAdapter.java
+++ MovieManager/source/net/sf/xmm/moviemanager/util/mac/OSXAdapter.java
@@ -15,12 +15,11 @@ import net.sf.xmm.moviemanager.util.FileUtil;
 import net.sf.xmm.moviemanager.util.Pictures;
 
 import com.apple.eawt.*;
-//import com.apple.mrj.MRJApplicationUtils;
+import com.apple.mrj.MRJApplicationUtils;
 
-public class OSXAdapter extends ApplicationAdapter /*implements com.apple.mrj.MRJAboutHandler */ {
+public class OSXAdapter extends ApplicationAdapter {
 
-
-	static Logger log = Logger.getRootLogger();
+	static Logger log = Logger.getLogger(OSXAdapter.class);
 
 	// pseudo-singleton model; no point in making multiple instances
 	// of the EAWT application or our adapter
@@ -30,49 +29,14 @@ public class OSXAdapter extends ApplicationAdapter /*implements com.apple.mrj.MR
 	private static DefaultApplication defaultApplication;
 	
 	// reference to the app where the existing quit, about, prefs code is
-	private DialogMovieManager						mainApp;
+	private DialogMovieManager mainApp;
 	
 	MovieManagerMenuBar menuBar = null;
-	
-	/*
-	private JMenuItem aboutButton;
-	private JMenuItem prefsButton;
-	private JMenuItem quitButton;
-*/
-	
+		
 	private OSXAdapter (DialogMovieManager inApp) {
 		mainApp = inApp;
-
-		System.err.println("menu bar:" + mainApp.getJMenuBar());
-
 		menuBar = (MovieManagerMenuBar) mainApp.getJMenuBar();
-
 		menuBar.setDatabaseComponentsEnable(false);
-
-		
-		
-		/*
-		JMenu helpMenu = mainApp.getJMenuBar().getMenu(4);
-		this.aboutButton = helpMenu.getItem(7);
-		aboutButton.setVisible(false);
-		helpMenu.getItem(0).setVisible(false);
-		helpMenu.remove(4);
-		helpMenu.add(new JPanel(), 4);
-		helpMenu.remove(6);
-		helpMenu.add(new JPanel(), 6);
-
-		JMenu toolsMenu = mainApp.getJMenuBar().getMenu(2);
-		this.prefsButton = toolsMenu.getItem(0);
-		prefsButton.setVisible(false);
-		toolsMenu.remove(1);
-		toolsMenu.add(new JPanel(), 1);
-
-		JMenu fileMenu = mainApp.getJMenuBar().getMenu(0);
-		this.quitButton = fileMenu.getItem(9);
-		quitButton.setVisible(false);
-		fileMenu.remove(8);
-		fileMenu.add(new JPanel(), 8);
-		 */
 	}
 
 	// The main entry-point for this functionality.  This is the only method
@@ -80,11 +44,9 @@ public class OSXAdapter extends ApplicationAdapter /*implements com.apple.mrj.MR
 	// reflection (see MyApp.java) 
 	public static void registerMacOSXApplication(DialogMovieManager inApp) {
 
-		System.err.println("registerMacOSXApplication");
-		System.err.println("theApplication:" + theApplication);
-
-		//System.setProperty("apple.laf.useScreenMenuBar", "true");  
-		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "MyApplication");
+		// This does not work on the latest versions of Java for OS X, so the name will be 
+		// displayed as the class name when run not run application bundle
+		//System.setProperty("com.apple.mrj.application.apple.menu.about.name", "MyApplication");
 		
 		if (theApplication == null) {
 			theApplication = new com.apple.eawt.Application();
@@ -95,29 +57,19 @@ public class OSXAdapter extends ApplicationAdapter /*implements com.apple.mrj.MR
 			theAdapter = new OSXAdapter(inApp);
 		}
 		
-		System.err.println("theAdapter:" + theAdapter);
-		
+		// Make sure that the about and preferences buttons in OS X works.
 		theApplication.addApplicationListener(theAdapter);
-		
-		//inApp.setApplication(defaultApplication);  
-		
+	
 		Image image = FileUtil.getImage("/images/film.png");
-		
 		BufferedImage bufferedImage = Pictures.toBufferedImage(image);
 		
+		// Sets the image in the bottom menu bar in OS x (even though it's NOT run in an application bundle)
 		defaultApplication.setApplicationIconImage(bufferedImage);
-		
-		//MRJApplicationUtils.registerAboutHandler(theAdapter);
-		
-		//BufferedImage newIcon = new BufferedImage(originalIcon.getWidth(), originalIcon.getHeight(), BufferedImage.TYPE_INT_ARGB);
-		//theApplication.setApplicationIconImage(image);
 	}
 
 	// implemented handler methods.  These are basically hooks into existing 
 	// functionality from the main app, as if it came over from another platform.
 	public void handleAbout(ApplicationEvent ae) {
-
-		System.err.println("handleAbout");
 
 		if (mainApp != null) {
 			menuBar.getAboutButton().doClick();
@@ -127,10 +79,6 @@ public class OSXAdapter extends ApplicationAdapter /*implements com.apple.mrj.MR
 		}
 	}
 	
-	public void handleAbout() {
-		System.err.println("handleAbout");
-	}
-		
 	public void handlePreferences(ApplicationEvent ae) {
 
 		if (mainApp != null) {
