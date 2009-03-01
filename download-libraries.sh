9bb6271103877b6bb6431e132e932773f5ba7675
Bro3 <Bro3@a86212f3-689c-4790-bbe1-32f6eb5c469e>
2009-03-01 14:26:16 +0000

    Script is outdated
    git-svn-id: svn://svn.code.sf.net/p/xmm/svn/trunk/MeDs@669 a86212f3-689c-4790-bbe1-32f6eb5c469e

--- download-libraries.sh
+++ download-libraries.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo "This script it outdated. Go download to the source package and copy the files in MovieManager/lib."
+exit
+
 echo "This script downloads the necessary libraries to run MeDs MovieManager."
 
 # Get current version from MovieManager.java to compose filename for sources
-VERFILE=`find . | grep /MovieManager.java`
+VERFILE=`find . | grep /MovieManagerConfig.java`
 CURRENTVERSION=`grep "String _version" $VERFILE | awk 'BEGIN { FS = "\"" } { print $2 }'`
 
 echo "Current CVS version ${CURRENTVERSION}"
