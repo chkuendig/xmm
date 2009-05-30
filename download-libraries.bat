6100246d4da91d273f4fefdb6a522fbb36115009
Bro3 <Bro3@a86212f3-689c-4790-bbe1-32f6eb5c469e>
2009-05-30 22:03:37 +0000

    git-svn-id: svn://svn.code.sf.net/p/xmm/svn/trunk/MeDs@1105 a86212f3-689c-4790-bbe1-32f6eb5c469e

--- download-libraries.bat
+++
@@ -1,7 +0,0 @@
-@echo You need to download additional libraries before you can run the software.
-@echo.
-@echo Please go to https://sourceforge.net/project/showfiles.php?group_id=83511#A&
-@echo and download the latest source code package.
-@echo.
-@echo Then copy the contents of MovieManager/lib/ in the zip file to MovieManager/lib.
-@echo.
