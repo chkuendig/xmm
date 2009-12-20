93f67e96187917a4afd6835e0b61ea04a39a8749
bro3 <bro3@a86212f3-689c-4790-bbe1-32f6eb5c469e>
2009-12-20 02:12:21 +0000

    git-svn-id: svn://svn.code.sf.net/p/xmm/svn/trunk/MeDs@1288 a86212f3-689c-4790-bbe1-32f6eb5c469e

--- MovieManager/source/net/sf/xmm/moviemanager/swing/extentions/filetree/AddSelectedFilesEventListener.java
+++
@@ -1,8 +0,0 @@
-package net.sf.xmm.moviemanager.swing.extentions.filetree;
-
-import java.util.EventListener;
-
-//A class must implement this interface to get MyEvents.
-public interface AddSelectedFilesEventListener extends EventListener {
-	public void addSelectedFilesEventOccurred(AddSelectedFilesEvent evt);
-}
