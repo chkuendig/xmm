848828fb098dcc2091caac408e151640d9d69267
bro3 <bro3@a86212f3-689c-4790-bbe1-32f6eb5c469e>
2009-12-30 16:07:56 +0000

    Added option removeQuotesOnSeriesTitles
    git-svn-id: svn://svn.code.sf.net/p/xmm/svn/trunk/MeDs@1348 a86212f3-689c-4790-bbe1-32f6eb5c469e

--- MovieManager/source/net/sf/xmm/moviemanager/http/HttpSettings.java
+++ MovieManager/source/net/sf/xmm/moviemanager/http/HttpSettings.java
@@ -1,3 +1,23 @@
+ /**
+ * @(#)HttpSettings.java
+ * 
+ * Copyright (2003) Bro3
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Boston, MA 02111.
+ *
+ * Contact: bro3@users.sourceforge.net
+ **/
+
 package net.sf.xmm.moviemanager.http;
 
 public class HttpSettings {
@@ -122,4 +142,14 @@ public class HttpSettings {
 	public void setAutoMoveAnAndA(boolean autoMoveAnAndA) {
 		this.autoMoveAnAndA = autoMoveAnAndA;
 	}
+	
+	public boolean getRemoveQuotesOnSeriesTitles() {
+		return removeQuotesOnSeriesTitles;
+	}
+
+	public void setRemoveQuotesOnSeriesTitles(boolean removeQuotesOnSeriesTitles) {
+		this.removeQuotesOnSeriesTitles = removeQuotesOnSeriesTitles;
+	}
+	
+	
 }
