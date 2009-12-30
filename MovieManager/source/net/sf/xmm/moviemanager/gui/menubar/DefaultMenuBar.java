61bb5c49482bd826a7e1c1da9bbf402a0ad0ab63
bro3 <bro3@a86212f3-689c-4790-bbe1-32f6eb5c469e>
2009-12-30 16:03:53 +0000

    Separated database handling from MovieManager.java into DatabaseHandler.java
    git-svn-id: svn://svn.code.sf.net/p/xmm/svn/trunk/MeDs@1342 a86212f3-689c-4790-bbe1-32f6eb5c469e

--- MovieManager/source/net/sf/xmm/moviemanager/gui/menubar/DefaultMenuBar.java
+++ MovieManager/source/net/sf/xmm/moviemanager/gui/menubar/DefaultMenuBar.java
@@ -1,3 +1,23 @@
+ /**
+ * @(#)DefaultMenuBar.java
+ * 
+ * Copyright (2003) Bro3
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Boston, MA 02111.
+ *
+ * Contact: bro3@users.sourceforge.net
+ **/
+
 package net.sf.xmm.moviemanager.gui.menubar;
 
 import java.awt.Color;
@@ -351,7 +371,7 @@ public class DefaultMenuBar extends JMenuBar implements MovieManagerMenuBar {
 		menuDatabase.add(menuItemConvertDatabase);
 
 		// When a new database is loaded, update list title
-        MovieManager.newDbHandler.addNewDatabaseLoadedEventListener(new NewDatabaseLoadedEventListener() {
+        MovieManager.getDatabaseHandler().getNewDatabaseLoadedHandler().addNewDatabaseLoadedEventListener(new NewDatabaseLoadedEventListener() {
         	public void newDatabaseLoaded(NewDatabaseLoadedEvent evt) {
         		validateConvertDatabaseItem();
 			}
