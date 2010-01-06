d6ed6af26e03f6a5dec66c25af8f048ba5451bf3
bro3 <bro3@a86212f3-689c-4790-bbe1-32f6eb5c469e>
2010-01-06 08:21:57 +0000

    Enter key will add new list
    git-svn-id: svn://svn.code.sf.net/p/xmm/svn/trunk/MeDs@1421 a86212f3-689c-4790-bbe1-32f6eb5c469e

--- MovieManager/source/net/sf/xmm/moviemanager/gui/DialogLists.java
+++ MovieManager/source/net/sf/xmm/moviemanager/gui/DialogLists.java
@@ -31,6 +31,7 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -146,6 +147,15 @@ public class DialogLists extends JDialog {
 		 panelAddNewField.setLayout(new GridBagLayout());
 		 JTextField textFieldAdd = new JTextField(22);
 		 textFieldAdd.setDocument(new DocumentRegExp("[\\p{Alnum}_\\s]*")); //$NON-NLS-1$
+		 textFieldAdd.addKeyListener(new KeyAdapter() {
+			 public void keyPressed(KeyEvent e) {
+
+				 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					 executeCommandAdd();
+				 }
+			 }
+		 });
+		 
 		 constraints = new GridBagConstraints();
 		 constraints.gridx = 0;
 		 constraints.gridy = 0;
