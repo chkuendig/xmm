34828449b19f3b644c2cb6dfa27f35a4f1d15951
bro3 <bro3@a86212f3-689c-4790-bbe1-32f6eb5c469e>
2010-01-17 15:14:49 +0000

    Now using DialogIMDbUpdate instead of DialogIMDbMultiAdd.
    git-svn-id: svn://svn.code.sf.net/p/xmm/svn/trunk/MeDs@1515 a86212f3-689c-4790-bbe1-32f6eb5c469e

--- MovieManager/source/net/sf/xmm/moviemanager/commands/guistarters/MovieManagerCommandDialogIMDB.java
+++ MovieManager/source/net/sf/xmm/moviemanager/commands/guistarters/MovieManagerCommandDialogIMDB.java
@@ -8,6 +8,7 @@ import org.apache.log4j.Logger;
 
 import net.sf.xmm.moviemanager.MovieManager;
 import net.sf.xmm.moviemanager.gui.DialogIMDB;
+import net.sf.xmm.moviemanager.gui.DialogIMDbUpdate;
 import net.sf.xmm.moviemanager.gui.DialogIMDbMultiAdd;
 import net.sf.xmm.moviemanager.models.ModelMovieInfo;
 import net.sf.xmm.moviemanager.util.GUIUtil;
@@ -35,12 +36,13 @@ public class MovieManagerCommandDialogIMDB {
 					ModelMovieInfo modelInfo = new ModelMovieInfo(false, true);
 					modelInfo.model.setTitle(movieTitle);
 
-					DialogIMDbMultiAdd dialogImdb = new DialogIMDbMultiAdd(modelInfo.model, true, movieTitle);
-				
+					DialogIMDbUpdate dialogIMDb = new DialogIMDbUpdate(modelInfo.model, movieTitle, true);
+					GUIUtil.showAndWait(dialogIMDb, true);
+					
 					tmpUrlKey = modelInfo.model.getUrlKey();
 					
-					cancel = dialogImdb.getCanceled();
-					cancelAll = dialogImdb.getAborted();
+					cancel = dialogIMDb.getCanceled();
+					cancelAll = dialogIMDb.getAborted();
 				}
 			});
 		} catch (InvocationTargetException e) {
