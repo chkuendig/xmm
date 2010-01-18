eb590d74aa9ab0bdf86c47eff628fb41d7019134
bro3 <bro3@a86212f3-689c-4790-bbe1-32f6eb5c469e>
2010-01-18 16:03:25 +0000

    Testing update function.
    git-svn-id: svn://svn.code.sf.net/p/xmm/svn/trunk/MeDs@1548 a86212f3-689c-4790-bbe1-32f6eb5c469e

--- MovieManager/source/net/sf/xmm/moviemanager/updater/AppUpdater.java
+++ MovieManager/source/net/sf/xmm/moviemanager/updater/AppUpdater.java
@@ -1,7 +1,11 @@
 package net.sf.xmm.moviemanager.updater;
 
+import org.apache.log4j.Logger;
+
 import net.sf.xmm.moviemanager.MovieManager;
 import net.sf.xmm.moviemanager.gui.DialogQuestion;
+import net.sf.xmm.moviemanager.http.IMDB;
+import net.sf.xmm.moviemanager.http.IMDbLib;
 import net.sf.xmm.moviemanager.util.SysUtil;
 
 import com.panayotis.jupidator.ApplicationInfo;
@@ -11,14 +15,29 @@ import com.panayotis.jupidator.UpdaterException;
 
 public class AppUpdater implements UpdatedApplication {
 
+	static Logger log = Logger.getLogger(AppUpdater.class);
+	
     public AppUpdater() {
         try {
         	
-            ApplicationInfo ap = new ApplicationInfo(
+        	System.err.println("numveric version:" + MovieManager.getConfig().sysSettings.getNumericalVersion());
+        	System.err.println("getVersion:" + MovieManager.getConfig().sysSettings.getVersion());
+        	
+        	int numericVersion = MovieManager.getConfig().sysSettings.getNumericalVersion();
+        	String version = MovieManager.getConfig().sysSettings.getVersion() + " - " + IMDbLib.getVersionString();
+        	
+        	System.err.println("VersionString:" + version);
+        	
+        	// IMDb is newer
+        	if (numericVersion < IMDbLib.getNumericVersion()) {
+        		numericVersion = IMDbLib.getNumericVersion();
+        	}
+        	
+        	ApplicationInfo ap = new ApplicationInfo(
                     SysUtil.getUserDir(),
                     SysUtil.getUserDir(),
-                    MovieManager.getConfig().sysSettings.getNumericalVersion(),
-                    MovieManager.getConfig().sysSettings.getVersion());
+                    "" + numericVersion,
+                    version);
             new Updater("http://xmm.sf.net/updates/update.xml", ap, this).actionDisplay();
         } catch (UpdaterException ex) {
             ex.printStackTrace();
@@ -26,7 +45,8 @@ public class AppUpdater implements UpdatedApplication {
     }
 
     public boolean requestRestart() {
-    	DialogQuestion q = new DialogQuestion("Restart", "May I restart!");
+    	
+    	DialogQuestion q = new DialogQuestion("Restart", "Restart necessary for changes to take effect. Restart now?");
     	q.setVisible(true);
     	
     	if (q.getAnswer())
@@ -36,6 +56,6 @@ public class AppUpdater implements UpdatedApplication {
     }
 
     public void receiveMessage(String message) {
-        System.err.println(message);
+        log.debug("Update message::" + message);
     }
 }
