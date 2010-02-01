11172bb905478420da721a057f6bc2d6fabb309d
bro3 <bro3@a86212f3-689c-4790-bbe1-32f6eb5c469e>
2010-02-01 21:30:26 +0000

    git-svn-id: svn://svn.code.sf.net/p/xmm/svn/trunk/MeDs@1710 a86212f3-689c-4790-bbe1-32f6eb5c469e

--- makejar.bat
+++
@@ -1,5 +0,0 @@
-@cd MovieManager
-@echo Deleting old jar file...
-@del MovieManager.jar
-jar cfm MovieManager.jar ../Manifest.txt codecs images queries net config/MovieManager.tmx
-@cd ..
--- makejar.sh
+++
@@ -1,4 +0,0 @@
-cd MovieManager
-rm MovieManager.jar
-jar cfm MovieManager.jar ../Manifest.txt codecs images queries net config/MovieManager.tmx
-cd ..
